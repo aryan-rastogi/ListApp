7a1b4f9a5e64f7e2318490e4d0a975d906f01be3
aryan-rastogi <75449029+aryan-rastogi@users.noreply.github.com>
2023-01-15 16:32:14 -0500

    initial commit

---
+++ BackEnd/routes/record.js
@@ -0,0 +1,77 @@
+const express = require("express");
+ 
+// recordRoutes is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /record.
+const recordRoutes = express.Router();
+ 
+// This will help us connect to the database
+const dbo = require("../db/conn");
+ 
+// This help convert the id from string to ObjectId for the _id.
+const ObjectId = require("mongodb").ObjectId;
+ 
+ 
+// This section will help you get a list of all the records.
+recordRoutes.route("/record").get(function (req, res) {
+ let db_connect = dbo.getDb("Lists");
+ db_connect
+   .collection("records")
+   .find({})
+   .toArray(function (err, result) {
+     if (err) throw err;
+     res.json(result);
+   });
+});
+ 
+// This section will help you get a single record by id
+recordRoutes.route("/record/:id").get(function (req, res) {
+ let db_connect = dbo.getDb();
+ let myquery = { _id: ObjectId(req.params.id) };
+ db_connect
+   .collection("records")
+   .findOne(myquery, function (err, result) {
+     if (err) throw err;
+     res.json(result);
+   });
+});
+ 
+// This section will help you create a new record.
+recordRoutes.route("/record/add").post(function (req, response) {
+ let db_connect = dbo.getDb();
+ let myobj = {
+   name: req.body.name,
+   category: req.body.category,
+   description: req.body.description,
+   link: req.body.link,
+   items: req.body.items,
+   date: new Date().toDateString(),
+   submissions: []
+ };
+ db_connect.collection("records").insertOne(myobj, function (err, res) {
+   if (err) throw err;
+   response.json(res);
+ });
+});
+ 
+// This section will help you update a record by id.
+recordRoutes.route("/addRanking/:id").post(function (req, response) {
+  console.log(req)
+ let db_connect = dbo.getDb();
+ let myquery = { _id: ObjectId(req.params.id) };
+ let newvalues = {
+   $set: {
+     submissions: req.body,
+   },
+ };
+ console.log("hey", req.body)
+ db_connect
+   .collection("records") 
+   .updateOne(myquery, newvalues, function (err, res) {
+     if (err) throw err;
+     console.log("1 document updated");
+     response.json(res);
+   });
+});
+ 
+module.exports = recordRoutes;
---
+++ FrontEnd/client/src/components/utils/card.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Card(props) {
+    const [data, setData] = useState(
+        {list :{
+        _id: "",
+        name: "",
+        date: "",
+        description: "",
+        link: "",
+        items: [],
+        submissions: []
+    }})
+
+    useEffect(() => {
+        console.log(data)
+        setData(props)
+        console.log(data)
+    })
+
+    return (
+        <div>
+            <h3>{data.list.name}</h3>
+            <p>{data.list.category}</p>
+            <p>{data.list.date}</p>
+            <p>{data.list.description}</p>
+            <a href={data.list.link}>{data.list.link}</a>
+            <br></br>
+            <Link className="btn btn-link" to={`/edit/${props.list._id}`}>Create Rank</Link>
+        </div>
+    )
+}
